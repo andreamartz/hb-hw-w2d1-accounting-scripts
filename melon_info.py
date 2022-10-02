7203519c5a7664f1ee8b2c272f80933e6d737831
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2022-10-01 22:18:55 -0500

    Restructure data file and rewrite print_melon_report

--- melon_info.py
+++ melon_info.py
@@ -1,18 +1,35 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_prices, melon_seedlessness 
+# from melons import melon_names, melon_prices, melon_seedlessness 
+from melons import melon_data 
+
+# # def print_melon(name, price, seedless):
+# def print_melon_report(attribute, value):
+
+#     """Print each melon with corresponding attribute information."""
+#     print(f"{attribute}: {value}")
 
 
-def print_melon(name, price, seedless):
+# for melon, data in melon_data.items():
+#     print("\n")
+#     print(f"{melon.upper()}")
+#     for attribute, value in data.items():
+#         print_melon(attribute, value)
+#     print("\n")
+#     print("**********************************")
+
+# def print_melon(name, price, seedless):
+def print_melon_report(melons):
+
     """Print each melon with corresponding attribute information."""
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
+    for melon, attributes in melons.items():
+        print("\n")
+        print(f"{melon.upper()}")
+        for attribute, value in attributes.items():
+            print(f"{attribute}: {value}")
+        print("\n")
+        print("**********************************")
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price}')
-
-
-for i in melon_names:
-    print_melon(melon_names[i], melon_prices[i], melon_seedlessness[i])
+print_melon_report(melon_data)
--- melons.py
+++ melons.py
@@ -1,23 +1,37 @@
-melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
-}
-
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
-
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
+melon_data = {
+    "Honeydew": {
+        "price": 0.99,
+        "is_seedless": True,
+        "flesh_color": None,
+        "rind_color": None,
+        "avg_weight": None,
+    },
+    "Crenshaw": {
+        "price": 2.00,
+        "is_seedless": False,
+        "flesh_color": None,
+        "rind_color": None,
+        "avg_weight": None,
+    },
+    "Crane": {
+        "price": 2.50,
+        "is_seedless": False,
+        "flesh_color": None,
+        "rind_color": None,
+        "avg_weight": None,
+    },
+    "Casaba": {
+        "price": 2.50,
+        "is_seedless": False,
+        "flesh_color": None,
+        "rind_color": None,
+        "avg_weight": None,
+    },
+    "Cantaloupe": {
+        "price": 0.99,
+        "is_seedless": False,
+        "flesh_color": None,
+        "rind_color": None,
+        "avg_weight": None,
+    },
 }
